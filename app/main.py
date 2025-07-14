360f4f2f4282efeb3a995402302f9deee893bff5
= <=>
2025-07-14 11:34:17 +0300

    objhknlm;,

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from typing import Callable
-
+from typing import Callable, Any
+from functools import wraps
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    saved_results = {}
+
+    @wraps(func)
+    def wrapper(*args: Any) -> Any:
+        if args in saved_results:
+            print("Getting from cache")
+            return saved_results[args]
+        print("Calculating new result")
+        result = func(*args)
+        saved_results[args] = result
+        return result
+
+    return wrapper
